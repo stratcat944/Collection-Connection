bdb63c6188ee5b8243a196367490d4e1ff4b6bac
Steve Domenico <stratcat944@gmail.com>
2015-08-13 10:37:17 -0600

    adding controllers

--- public/main.js
+++ public/main.js
@@ -7,26 +7,6 @@ collectConnect.config(function($routeProvider){
 			templateUrl	: '/templates/view1', 		// View
 			controller 	: 'view1'             // Controller
 		})
-		.when('/view1',{
-			templateUrl	: '/templates/view1', 		// View
-			controller 	: 'view1'             // Controller
-		})
-		.when('/view2',{
-			templateUrl : '/templates/view2',
-			controller 	: 'view2'
-		})
-		.when('/view3',{
-			templateUrl : '/templates/view3',
-			controller 	: 'view3'
-		})
-		.when('/view4',{
-			templateUrl : '/templates/view4',
-			controller 	: 'view4'
-		})
-		.when('/view5',{
-			templateUrl : '/templates/view5',
-			controller 	: 'view5'
-		})
 
 });
 
@@ -36,29 +16,14 @@ collectConnect.controller('view1', function($scope){
 	$scope.createArtistAlbum = function(){
 		console.log('create', $scope.artistAlbum);
 	}
-})
-
-collectConnect.controller('view2', function($scope){
 	$scope.personnel = [{}]
 	$scope.addPerson = function(){
 		$scope.personnel.push({})
 	}
-	$scope.createPerson = function(){
-		console.log($scope.personnel);
+	$scope.tracks = [{}]
+	$scope.addTrack = function(){
+		$scope.tracks.push({})
 	}
-	console.log('View 2 Controller!');
-})
-
-collectConnect.controller('view3', function($scope){
-	console.log('View 3 Controller!');
-})
-
-collectConnect.controller('view4', function($scope){
-	console.log('View 4 Controller!');
-})
-
-collectConnect.controller('view5', function($scope){
-	console.log('View 5 Controller!');
 })
 
 
