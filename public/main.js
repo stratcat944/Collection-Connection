9c53dd0b028586d19ae71296a4d547b7a2a55370
Steve Domenico <stratcat944@gmail.com>
2015-08-13 11:56:40 -0600

    submit working

--- public/main.js
+++ public/main.js
@@ -13,16 +13,17 @@ collectConnect.config(function($routeProvider){
 
 collectConnect.controller('view1', function($scope){
 	console.log('View 1 Controller!');
-	$scope.createArtistAlbum = function(){
-		console.log('create', $scope.artistAlbum);
-	}
-	$scope.personnel = [{}]
+	$scope.album = {}
+	$scope.album.personnel = [{}]
 	$scope.addPerson = function(){
-		$scope.personnel.push({})
+		$scope.album.personnel.push({})
 	}
-	$scope.tracks = [{}]
+	$scope.album.tracks = [{}]
 	$scope.addTrack = function(){
-		$scope.tracks.push({})
+		$scope.album.tracks.push({})
+	}
+	$scope.createAlbum = function(){
+		console.log('create', $scope.album);
 	}
 })
 
