299927c374c05748766985827e94f67102ebd741
Steve Domenico <stratcat944@gmail.com>
2015-08-13 16:05:25 -0600

    bells and whistles added

--- public/main.js
+++ public/main.js
@@ -14,20 +14,31 @@ collectConnect.config(function($routeProvider){
 collectConnect.controller('view1', function($scope, $http){
 	console.log('View 1 Controller!');
 	$scope.album = {}
+
+
 	$scope.album.personnel = [{}]
 	$scope.addPerson = function(){
 		$scope.album.personnel.push({})
 	}
+
+
 	$scope.album.tracks = [{}]
 	$scope.addTrack = function(){
 		$scope.album.tracks.push({})
 	}
+
+
 	$scope.createAlbum = function(){
 		console.log('create', $scope.album);
 		$http.post('/albums/create', $scope.album)
 		.then(function(returnData){
 			console.log(returnData);
-			$scope.album = {}
+			
+			$scope.album = {
+				personnel:[{}],
+				tracks: [{}]
+			}
+			
 		})
 	}
 })
